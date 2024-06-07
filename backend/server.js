8941c0515dcf165cd34215573422fe50acb88b95
Ankit Kumar <158794632+Oyeankit6@users.noreply.github.com>
2024-06-07 13:05:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import orderRouter from "./Routes/orderRoute.js";
 
 ////App config
 const app = express();
-const port = 4000;
+const port = process.env.PORT ||4000;
 
 /// middle ware
 app.use(express.json());
