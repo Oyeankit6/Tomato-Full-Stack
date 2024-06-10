79ea167a74ff51d4cd82832aab4531add71e31e2
Ankit Kumar <158794632+Oyeankit6@users.noreply.github.com>
2024-06-10 08:40:37 +0530

    Update StoreContext.jsx

--- frontend/src/components/Context/StoreContext.jsx
+++ frontend/src/components/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://tomato-full-stack-3b8c.vercel.app/";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
